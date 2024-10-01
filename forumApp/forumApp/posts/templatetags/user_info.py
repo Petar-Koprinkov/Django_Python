66ff2c20dd047fe662c80a2bd8e64778edfd299d
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-10-01 17:30:36 +0300

    fixed: Corrected CSS

--- forumApp/forumApp/posts/templatetags/user_info.py
+++ forumApp/forumApp/posts/templatetags/user_info.py
@@ -5,8 +5,6 @@ register = template.Library()
 
 @register.inclusion_tag(filename='forum/include/user_info.html', takes_context=True)
 def user_info(context):
-    print(context)
-    print(context.request.user.username)
     if context.request.user.is_authenticated:
         return {
             'username': context.request.user,
